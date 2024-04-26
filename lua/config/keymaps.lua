66b8bd70c7bacc51f58cafac7ec72e667700cdaa
Dave Russell <daverussell1309@gmail.com>
2024-04-27 02:22:30 +0700

    feat: add neovide options and change add border to lazyterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Bordered terminalvim
+local LazyTerm = function()
+  LazyVim.terminal(nil, {
+    border = "rounded",
+    cwd = LazyVim.root(),
+  })
+end
+
+vim.keymap.set("n", "<C-/>", LazyTerm, { desc = "Terminal (Root Dir)" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,46 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Neovide Options
+if vim.g.neovide then
+  -- Font
+  vim.o.guifont = "Cascadia Code:h10.875:b"
+  vim.g.neovide_scale_factor = 1
+
+  -- Padding
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  -- Cursor
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_length = 0
+  vim.g.neovide_cursor_antialiasing = false
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_unfocused_outline_width = 0
+
+  -- Scroll
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_no_idle = false
+
+  -- UI
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_transparency = 0.75
+end
+
+-- Settin Up Terminal
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
+end
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
