4db126dfdedf770d4d9e0a6db0c0a4a5539cb02d
Dave Russell <daverussell1309@gmail.com>
2024-04-28 09:46:07 +0700

    feat: change dashboard ui

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -41,4 +41,108 @@ return {
       }
     }
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "shfmt",
+        "prettier",
+        "eslint-lsp",
+        "emmet-ls",
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server"
+      })
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- Inherited
+        -- "bash",
+        -- "c",
+        -- "diff",
+        -- "html",
+        -- "javascript",
+        -- "jsdoc",
+        -- "json",
+        -- "jsonc",
+        -- "lua",
+        -- "luadoc",
+        -- "luap",
+        -- "markdown",
+        -- "markdown_inline",
+        -- "python",
+        -- "query",
+        -- "regex",
+        -- "toml",
+        -- "tsx",
+        -- "typescript",
+        -- "vim",
+        -- "vimdoc",
+        -- "xml",
+        -- "yaml",
+        "css",
+        "scss",
+        "gitignore",
+        "sql"
+      },
+    }
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function()
+      local logo = [[
+  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+        ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      local opts = {
+        theme = "doom",
+        hide = {
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+          center = {
+            { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
+            { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
+            { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+            { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "", "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+      return opts
+    end,
+  },
 }
